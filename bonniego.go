946bef1d20c94ae1ddc141921005db4fc480a0a0
Brian Cunnie <bcunnie@pivotal.io>
2014-11-12 13:16:15 -0800

    Time and command line args, beginnings

--- bonniego.go
+++ bonniego.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"time"
+)
 
 func main() {
-	PrintResults("Hello, world.\n")
+	var ip = flag.Int("flagname", 1234, "help message for flagname")
+	flag.Parse()
+	fmt.Printf("ip: %d\n", *ip)
+	PrintResults("Hello, world. The time is")
 }
 
 func PrintResults(results string) {
-	fmt.Printf("asdf" + results)
+	var now = time.Now()
+	fmt.Printf("%s, %d\n", results, now.UnixNano())
 	// results
 }
