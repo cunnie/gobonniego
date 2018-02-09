3babd38d9224ac9397e30b6aa3bac2e62b8c094e
Brian Cunnie <brian.cunnie@gmail.com>
2018-02-09 06:37:09 -0800

    First benchmark complete! Write performance.
    - untested, that's for future
    - single-threaded

--- bonniego.go
+++ bonniego.go
@@ -1,20 +1,44 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"time"
+	"os"
+	"bufio"
+	"crypto/rand"
+	"math"
 )
 
 func main() {
-	var ip = flag.Int("flagname", 1234, "help message for flagname")
-	flag.Parse()
-	fmt.Printf("ip: %d\n", *ip)
-	PrintResults("Hello, world. The time is")
-}
+	//var ip = flag.Int("flagname", 1234, "help message for flagname")
+	//flag.Parse()
+	//fmt.Printf("ip: %d\n", *ip)
 
-func PrintResults(results string) {
-	var now = time.Now()
-	fmt.Printf("%s, %d\n", results, now.UnixNano())
-	// results
+
+	f, err := os.Create("/tmp/dat2")
+	if err != nil {
+		panic("couldn't open")
+	}
+	defer f.Close()
+
+	b := make([]byte, 0x1<<16)
+	_, err = rand.Read(b)
+	if err != nil {
+		panic("couldn't rand")
+	}
+
+	w := bufio.NewWriter(f)
+	start := time.Now()
+
+	n, err := w.Write(b)
+	if err != nil {
+		panic("couldn't write")
+	}
+
+	w.Flush()
+	finish := time.Now()
+	duration := finish.Sub(start)
+	fmt.Printf("wrote %d bytes\n", n)
+	fmt.Printf("took %d nanoseconds\n", duration.Nanoseconds())
+	fmt.Printf("throughput %0.2f MiB/s\n", float64(len(b))/float64(duration.Seconds())/math.Exp2(20))
 }
